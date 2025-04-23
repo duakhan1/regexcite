676e49532aa4f643a3ecddf6a9003cfa44a645da
Dua Khan <duakhan@student.ubc.ca>
2025-04-22 17:05:57 -0700

    Initial commit

---
+++ R/strsplit1.R
@@ -0,0 +1,16 @@
+#' Split a string once
+#'
+#' A helper to split a string into a character vector instead of a list.
+#'
+#' @param x A character vector of length 1.
+#' @param split A character string to split `x` on.
+#'
+#' @return A character vector of the split string.
+#' @export
+#'
+#' @examples
+#' strsplit1("a,b,c", ",")
+strsplit1 <- function(x, split) {
+  strsplit(x, split = split)[[1]]
+}
+
